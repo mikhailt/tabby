f542a43f0b446230b0ead65d5e90c0aac70a107d
mikhailt <mikhailt@tratata.local>
2010-10-25 02:26:39 +0400

    two minor changes:   .gitignore added   font doesnt change anymore when something is inserted

--- tabby.go
+++ tabby.go
@@ -1,53 +1,58 @@
 package main
 
 import (
-//	"os"
+	//"os"
 	"gtk"
-//	"gdkpixbuf"
-//	"path"
-  "fmt"
-//"reflect"
-  "strings"
-//"runtime"
-//"unsafe"
+	//"gdkpixbuf"
+	//"path"
+	"fmt"
+	//"reflect"
+	"strings"
+	//"runtime"
+	//"unsafe"
 )
 
 func buffer_changed(buf *gtk.GtkTextBuffer) {
-  fmt.Printf("beep\n");
+	fmt.Printf("changed\n")
 }
 
 
-
 func highlight_instances(buf *gtk.GtkTextBuffer) {
-  var be, en gtk.GtkTextIter
-  var start, end gtk.GtkTextIter
+	var be, en gtk.GtkTextIter
+	var start, end gtk.GtkTextIter
 
-  buf.GetStartIter(&start)
-  buf.GetEndIter(&end)
+	buf.GetStartIter(&start)
+	buf.GetEndIter(&end)
 
-  buf.RemoveTagByName("instance", &start, &end)
+	buf.RemoveTagByName("global_font", &start, &end)
+	buf.ApplyTagByName("global_font", &start, &end)
+	buf.RemoveTagByName("instance", &start, &end)
 
-  if buf.GetHasSelection() {
-    buf.GetIterAtMark(&be, buf.GetMark("selection_bound"))
-    buf.GetIterAtMark(&en, buf.GetMark("insert"))
-    selection := buf.GetSlice(&be, &en, false)
-    sel_len := len(selection)
+	if buf.GetHasSelection() {
+		fmt.Printf("beep\n")
+		buf.GetIterAtMark(&be, buf.GetMark("selection_bound"))
+		buf.GetIterAtMark(&en, buf.GetMark("insert"))
+		selection := buf.GetSlice(&be, &en, false)
+		sel_len := len(selection)
+		if (20 < sel_len) || (2 > sel_len ) {
+			return
+		}
 
-    text := buf.GetSlice(&start, &end, false)
-    shift := 0
-    for be_ind := 0; ; {
-      be_ind = strings.Index(text, selection)
-      if (-1 == be_ind) {
-        break
-      }
-      shift += be_ind
-      buf.GetIterAtOffset(&be, shift);
-      buf.GetIterAtOffset(&en, shift + sel_len);
-      buf.ApplyTagByName("instance", &be, &en)
-      text = text[be_ind + sel_len:]
-      shift += sel_len
-    }
-  }
+		text := buf.GetSlice(&start, &end, false)
+		shift := 0
+		for be_ind := 0; ; {
+			be_ind = strings.Index(text, selection)
+			if -1 == be_ind {
+				break
+			}
+			shift += be_ind
+			buf.GetIterAtOffset(&be, shift)
+			buf.GetIterAtOffset(&en, shift+sel_len)
+			buf.ApplyTagByName("instance", &be, &en)
+			text = text[be_ind+sel_len:]
+			shift += sel_len
+		}
+	}
 }
 
 func main() {
@@ -113,20 +118,22 @@ func main() {
 	var start, end gtk.GtkTextIter
 	buffer := textview.GetBuffer()
 
-  buffer.Connect("mark-set", func() {
-    highlight_instances(buffer)
-  }, nil)
-  buffer.Connect("changed", func() {
-    buffer_changed(buffer)
-  }, nil)
+	buffer.Connect("mark-set", func() {
+		highlight_instances(buffer)
+	},
+		nil)
+	buffer.Connect("changed", func() {
+		buffer_changed(buffer)
+	},
+		nil)
 	buffer.GetStartIter(&start)
 	buffer.Insert(&start, "Hello\nSome more words\n#include \"iostream\"\nHello")
 	buffer.GetEndIter(&end)
 	buffer.Insert(&end, "World!")
 	buffer.CreateTag("instance", map[string]string{
 		"background": "#CCCC99"})
-  buffer.CreateTag("global_font", map[string]string{
-    "font": "Monospace Regular 10"})
+	buffer.CreateTag("global_font", map[string]string{
+		"font": "Monospace Regular 10"})
 	buffer.GetStartIter(&start)
 	buffer.GetEndIter(&end)
 	buffer.ApplyTagByName("global_font", &start, &end)
