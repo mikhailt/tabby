6b1f3668f22a94c2ae048768ff83da4e58566f0e
mikhailt <mikhailt@tratata.(none)>
2011-02-03 02:00:20 +0300

    save cursor position in session

--- src/session.go
+++ src/session.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"bufio"
 	"regexp"
+	"strings"
+	"strconv"
 )
 
 type IgnoreMap map[string]*regexp.Regexp
@@ -56,7 +58,15 @@ func get_stack_set() (map[string]int, []string, int) {
 	return m, list, list_size
 }
 
+func get_file_info(file string) string {
+	rec := file_map[file]
+	be_str := strconv.Itoa(rec.sel_be)
+	en_str := strconv.Itoa(rec.sel_en)
+	return file + ":" + be_str + ":" + en_str + "\n"
+}
+
 func session_save() {
+	file_save_current()
 	file, _ := os.Open(os.Getenv("HOME")+"/.tabby", os.O_CREAT|os.O_WRONLY, 0644)
 	if nil == file {
 		tabby_log("unable to save session")
@@ -68,13 +78,13 @@ func session_save() {
 	for k, _ := range file_map {
 		_, found := stack_set[k]
 		if (false == found) && file_is_saved(k) {
-			file.WriteString(k + "\n")
+			file.WriteString(get_file_info(k))
 		}
 	}
 	// Dump files from stack in the right order. Last file should be last in the
 	// list of files in .tabby file.
 	for y := list_size - 1; y >= 0; y-- {
-		file.WriteString(list[y] + "\n")
+		file.WriteString(get_file_info(list[y]))
 	}
 	file.Close()
 }
@@ -96,7 +106,12 @@ func session_restore() {
 	defer file.Close()
 	var str string
 	for next_string_from_reader(reader, &str) {
-		session_open_and_read_file(str)
+		split_str := strings.Split(str, ":", 3)
+		session_open_and_read_file(split_str[0])
+		be, _ := strconv.Atoi(split_str[1])
+		en, _ := strconv.Atoi(split_str[2])
+		file_map[split_str[0]].sel_be = be
+		file_map[split_str[0]].sel_en = en
 	}
 	ignore = make(IgnoreMap)
 	reader, file = take_reader_from_file(os.Getenv("HOME") + "/.tabbyignore")
