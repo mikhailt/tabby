b75d4bddf516f1fa3cce80e6e7d141783ac373cc
Mikhail Trushnikov <bydyww@gmail.com>
2011-05-24 00:29:10 +0400

    replace spaces by tabs

--- src/session.go
+++ src/session.go
@@ -108,11 +108,11 @@ func session_restore() {
 	for next_string_from_reader(reader, &str) {
 		split_str := strings.Split(str, ":", 3)
 		if session_open_and_read_file(split_str[0]) {
-		        be, _ := strconv.Atoi(split_str[1])
-		        en, _ := strconv.Atoi(split_str[2])
-		        file_map[split_str[0]].sel_be = be
-		        file_map[split_str[0]].sel_en = en
-                }
+			be, _ := strconv.Atoi(split_str[1])
+			en, _ := strconv.Atoi(split_str[2])
+			file_map[split_str[0]].sel_be = be
+			file_map[split_str[0]].sel_en = en
+		}
 	}
 	ignore = make(IgnoreMap)
 	reader, file = take_reader_from_file(os.Getenv("HOME") + "/.tabbyignore")
