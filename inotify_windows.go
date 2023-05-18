8e490f8c77b7c842c234e513824577f86751dfb4
misha-agenthub <132542866+misha-agenthub@users.noreply.github.com>
2023-05-18 16:29:24 -0700

    inotify_windows.go - commit created by https://agenthub.dev/agent?run_id=f7186b5c-9f08-4991-96cb-509d985a6f4b

--- inotify_windows.go
+++ inotify_windows.go
@@ -1,21 +1,27 @@
 package main
 
+// Initializes inotify
 func init_inotify() {
 }
 
+// Remove watch with given name from inotify
 func inotify_rm_watch(name string) {
 }
 
+// Observe inotify events
 func inotify_observe() {
 }
 
+// Collect inotify buffer events in a map
 func inotify_observe_collect(buf []byte) map[string]int {
 	return make(map[string]int)
 }
 
+// Display dialog for inotify events
 func inotify_dialog(s map[string]int) bool {
 	return false
 }
 
+// Add watch with given name to inotify
 func inotify_add_watch(name string) {
 }
